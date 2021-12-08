5297138968d0516853717c3f5cbeb04b905aec00
Dunahoo, Nathan <nathan-dunahoo@uiowa.edu>
2021-12-08 17:39:08 -0600

    Added requirements.txt

---
+++ commands/CustomUserCommands.py
@@ -0,0 +1,16 @@
+import discord
+from discord.ext import commands
+import os
+
+class CustomUserCommands(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command()
+    async def pimmy(self, ctx):
+        file = r'./media/FENNECFRIDAY.mp4'
+        if os.path.exists(file):
+            await ctx.send(file=discord.File(file))
+
+def setup(client):
+    client.add_cog(CustomUserCommands(client))
--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 import os
 from discord.ext import commands
-from core import ErrorHandling
+from core.ErrorHandling import ErrorHandler
 
 client = commands.Bot(command_prefix='!')
 
-cog_files = ['TrialCommands', 'CustomUserCommands']
+cog_files = ['TrialCommands', 'CustomUserCommands']  # Define command file here
 for cog_file in cog_files:
     client.load_extension(f"commands.{cog_file}")
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
-    client.add_cog(ErrorHandling.ErrorHandler(client))
+    client.add_cog(ErrorHandler(client))
 
 @client.command()
 @commands.has_permissions(administrator=True)
